9b3e2d9382ead354435a193ca8d0b7e74d1fbbb1
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-08-06 00:10:03 +0300

    Init repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,29 @@
 require(`dotenv`).config({
-  path: `.env`,
+  path: `.env`
 })
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emma - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Fall and Winter Art`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emma`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emma - @lekoarts/gatsby-theme-emma`,
-        short_name: `Emma`,
-        description: `Minimalistic bright portfolio with full-width grid and large images`,
+        name: `Fall and Winter Art`,
+        short_name: `FaWa`,
+        description: `Fall and Winter Art`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#b83280`,
@@ -32,17 +32,17 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
+    `gatsby-plugin-netlify`
+  ]
 }
