83b691d86b8b742fb07d8a99ae7706fff32ab426
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-09-03 00:09:56 +0300

    Add shadowing

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,20 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Fall and Winter Art`
+    // Used for the title template on pages other than the index site
+    siteTitle: `Fall and Winter Art`,
+    // Default title of the page
+    siteTitleAlt: `Fall and Winter Art`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Fall and Winter Art`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://www.fallandwinterart.com/`,
+    // Used for SEO
+    siteDescription: `Fall and Winter Art`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`
   },
   plugins: [
     {
@@ -15,6 +28,12 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `fallandwinterart`
       }
     },
     `gatsby-plugin-sitemap`,
@@ -30,14 +49,14 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
+            src: `/android-chrome-192x192.jpg`,
             sizes: `192x192`,
-            type: `image/png`
+            type: `image/jpg`
           },
           {
-            src: `/android-chrome-512x512.png`,
+            src: `/android-chrome-512x512.jpg`,
             sizes: `512x512`,
-            type: `image/png`
+            type: `image/jpg`
           }
         ]
       }
